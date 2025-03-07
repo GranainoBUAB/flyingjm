faee9cff07d087423d0ec72531153e9431867b29
GranainoBUAB <josemiguel@quesada.eu>
2025-03-07 15:28:48 +0100

    feat: add CRUD for plane in API

---
+++ app/Http/Controllers/Api/PlaneController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Plane;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PlaneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $planes = Plane::all();
+        return response()->json($planes, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $plane = Plane::create([
+            'registration' => $request->registration,
+            'imgplane' => $request->imgplane,
+            'seats' => $request->seats,
+        ]);
+
+        //$plane->save();
+        return response()->json($plane, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $plane = Plane::find($id);
+        return response()->json($plane, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $plane = Plane::find($id);
+
+        $plane->update([
+            'registration' => $request->registration,
+            'imgplane' => $request->imgplane,
+            'seats' => $request->seats,
+        ]);
+
+        //$plane->save();
+        return response()->json($plane, 200);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $plane = Plane::find($id);
+        $plane->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PlaneController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/planes',[PlaneController::class,'index'])->name('apiplaneshome');
+Route::delete('/planes/{id}',[PlaneController::class,'destroy'])->name('apiplanesdestroy');
+Route::post('/planes', [PlaneController::class, 'store'])->name('apiplanesstore');
+Route::put('/planes/{id}',[PlaneController::class,'update'])->name('apiplanesupdate');
