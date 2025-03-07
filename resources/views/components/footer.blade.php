ac23618b644bf6a4740d5b0fd24fb0aed2b39860
GranainoBUAB <josemiguel@quesada.eu>
2025-03-07 13:16:24 +0100

    change info

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,43 +1,26 @@
 <hr>
 <div class="container">
     <footer class="d-flex flex-wrap justify-content-around align-items-center my-3">
-        <div class="col-1 col-md-4 d-flex align-items-center">
-            <span class="follow">Siguenos</span>
-            <img class="icon_arrow follow ms-2" src="{{ url('/img/right-arrow.svg') }}" alt="Pixel perfect">
-        </div>
         <ul class="nav col-8 col-md-4 justify-content-center list-unstyled d-flex">
             <li class="ms-3 ms-sm-0">
-                <a class="text-muted" href="https://twitter.com/hangarrebelde" target="_blank">
-                    <img class="bi" width="24" height="24" src="{{ url('/img/twitter.svg') }}" alt="Pixel perfect">
-                </a>
+                <img class="bi" width="24" height="24" src="{{ url('/img/twitter.svg') }}" alt="Pixel perfect">
             </li>
             <li class="ms-3">
-                <a class="text-muted" href="https://www.instagram.com/hangarrebelde/" target="_blank">
-                    <img class="bi" width="24" height="24" src="{{ url('/img/instagram.svg') }}" alt="Pixel perfect">
-                </a>
+                <img class="bi" width="24" height="24" src="{{ url('/img/instagram.svg') }}"
+                    alt="Pixel perfect">
             </li>
             <li class="ms-3">
-                <a class="text-muted" href="https://www.facebook.com/hangarrebelde/" target="_blank">
-                    <img class="bi" width="24" height="24" src="{{ url('/img/facebook.svg') }}" alt="Pixel perfect">
-                </a>
+                <img class="bi" width="24" height="24" src="{{ url('/img/facebook.svg') }}"
+                    alt="Pixel perfect">
             </li>
             <li class="ms-3">
-                <a class="text-muted" href="https://discord.com/invite/4bd5D8T" target="_blank">
-                    <img class="bi" width="24" height="24" src="{{ url('/img/discord.svg') }}" alt="Pixel perfect">
-                </a>
+                <img class="bi" width="24" height="24" src="{{ url('/img/discord.svg') }}"
+                    alt="Pixel perfect">
             </li>
             <li class="ms-3">
-                <a class="text-muted" href="https://api.whatsapp.com/send/?phone=674620999&text&app_absent=0" target="_blank">
-                    <img class="bi" width="24" height="24" src="{{ url('/img/whatsapp.svg') }}" alt="Pixel perfect">
-                </a>
+                <img class="bi" width="24" height="24" src="{{ url('/img/whatsapp.svg') }}"
+                    alt="Pixel perfect">
             </li>
         </ul>
-
-        <div class="col-6 col-md-4 d-flex align-items-center justify-content-end location">
-            <a class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1" href="http://share.here.com/r/mylocation/e-eyJuYW1lIjoiSGFuZ2FyIFJlYmVsZGUiLCJhZGRyZXNzIjoiQXJ0dXJvIEFsdmFyZXogQnV5bGxhIDMsIE92aWVkbyIsImxhdGl0dWRlIjo0My4zNTk0NCwibG9uZ2l0dWRlIjotNS44NTkyLCJwcm92aWRlck5hbWUiOiJmYWNlYm9vayIsInByb3ZpZGVySWQiOjIyNDAwNDgzMDk0MzI3Mn0=?link=addresses&fb_locale=es_ES&ref=facebook&fbclid=IwAR03yjp56mDMl3FjRHnxo6Jfsm4k5u5Iipl2FTSISWn13214-FGacM1NPwY" target="_blank">
-                <img class="bi" width="28" height="24" src="{{url('/img/location-pin.svg')}}" alt="Pixel perfect">
-            
-            <span class="text">como Llegar</span></a>
-        </div>
     </footer>
 </div>
