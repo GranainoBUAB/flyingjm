eeb71e347607fe9c962d964224e2ce59d0c0bc6f
GranainoBUAB <josemiguel@quesada.eu>
2025-03-10 07:41:31 +0100

    fix: change branch

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PlaneController;
-
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +14,4 @@ Route::get('/planes',[PlaneController::class,'index'])->name('apiplaneshome');
 Route::delete('/planes/{id}',[PlaneController::class,'destroy'])->name('apiplanesdestroy');
 Route::post('/planes', [PlaneController::class, 'store'])->name('apiplanesstore');
 Route::put('/planes/{id}',[PlaneController::class,'update'])->name('apiplanesupdate');
+
